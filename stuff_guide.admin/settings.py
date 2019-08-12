960229328ebe91f080710f11fdfb18e88b8dc6ad
Arnold_Lee <runlin.li@kaiqigu.net>
2019-08-12 11:21:27 +0800

    1

--- stuff_guide.admin/settings.py
+++ stuff_guide.admin/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'django_admin.urls'
+ROOT_URLCONF = 'stuff_guide.admin.urls'
 
 TEMPLATES = [
     {
